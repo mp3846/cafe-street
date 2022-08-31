55bf548c233d2669cd294a14f24be099c1d7f8d1
MP <mpourmorad@gmail.com>
2022-09-01 00:36:21 +0430

    Add magnifier icon to search input

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,14 +4,18 @@ import icon_cart from '../img/icons/icons_cart.svg'
 
 const Navbar = memo(() => (
 	<div className='navbar-container'>
-		<img className='navbar-logo cp' src={cafe_street_logo} alt='cafe street logo' />
+		<div className='navbar-logo cp'>
+			<img src={cafe_street_logo} alt='cafe street logo' />
+		</div>
 		<div className='navbar-items'>
 			<span className='navbar-item'>About us</span>
 			<span className='navbar-item'>Our Product</span>
 			<span className='navbar-item'>Delivery</span>
 		</div>
-		<div className='navbar-search'></div>
-		<img className='navbar-cart-icon cp' src={icon_cart} alt='cart icon' />
+		<div className='navbar-search'>
+			<input placeholder='Cappuccino' />
+		</div>
+		<img className='navbar-cart cp' src={icon_cart} alt='cart icon' />
 	</div>
 ))
 
