c50f97fba3f7d3fe309b5200871b67a8af52e3ed
MP <mpourmorad@gmail.com>
2022-09-03 15:29:14 +0430

    Add delivery section

--- src/components/delivery.jsx
+++ src/components/delivery.jsx
@@ -1,5 +1,44 @@
 import { memo } from 'react'
+import choose_coffee from '../img/choose_coffee.webp'
+import coffee_truck from '../img/coffee-truck.webp'
+import coffee_cup from '../img/coffee-cup.webp'
 
-const Delivery = memo(() => <div className='delivery-container'>delivery</div>)
+const items = [
+	{
+		title: 'choose your coffee',
+		description: 'there are 20+ coffee for you',
+		img: choose_coffee,
+		alt: 'choose coffee'
+	},
+	{
+		title: 'we delivery it to you',
+		description: 'choose delivery service',
+		img: coffee_truck,
+		alt: 'coffee delivery service'
+	},
+	{
+		title: 'enjoy your coffee',
+		description: 'have a nice day',
+		img: coffee_cup,
+		alt: 'enjoy coffee'
+	}
+]
+
+const Delivery = memo(() => (
+	<div className='delivery-container'>
+		<h2 className='delivery-title'>
+			How to use delivery <span>service</span>
+		</h2>
+		<div className='delivery-items-wrapper'>
+			{items.map((item) => (
+				<div className='delivery-item'>
+					<img src={item.img} alt={item.alt} />
+					<span>{item.title}</span>
+					<span>{item.description}</span>
+				</div>
+			))}
+		</div>
+	</div>
+))
 
 export default Delivery
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,5 @@
 import { memo } from 'react'
+import Delivery from './delivery'
 import Hero from './hero'
 import Popular from './popular'
 
@@ -6,6 +7,7 @@ const Home = memo(() => (
 	<div className='home-container'>
 		<Hero />
 		<Popular />
+		<Delivery />
 	</div>
 ))
 
